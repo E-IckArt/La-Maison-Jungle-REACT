0c4e1c598fa4f94b27f386f45b04a4e6f3d3075c
Emilie Ickart <emi.ickart@gmail.com>
2022-05-31 10:02:17 +0200

    Modify Banner style

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -16,8 +16,8 @@ const Banner = () => {
             <div className="lmj-banner-row">
                 <img src={logo} alt="La maison jungle" className="lmj-logo" />
                 <h1 className="lmj-title">{title}</h1>
+                {recommendation}
             </div>
-            {recommendation}
         </div>
     );
 };
