bb0cb65cb06116db49c892c84d8c5c6ebce642c1
Emilie Ickart <emi.ickart@gmail.com>
2022-06-07 16:03:29 +0200

    Ft-Update plantList Images

--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -1,4 +1,12 @@
+import basil from '../assets/basil.jpg';
+import cactus from '../assets/cactus.jpg';
+import calathea from '../assets/calathea.jpg';
+import lyrata from '../assets/lyrata.jpg';
+import mint from '../assets/mint.jpg';
 import monstera from '../assets/monstera.jpg';
+import olivier from '../assets/olivier.jpg';
+import pothos from '../assets/pothos.jpg';
+import succulent from '../assets/succulent.jpg';
 
 export const plantList = [
     {
@@ -16,7 +24,7 @@ export const plantList = [
         id: '2ab',
         light: 3,
         water: 1,
-        cover: monstera,
+        cover: lyrata,
     },
     {
         name: 'pothos argenté',
@@ -24,15 +32,15 @@ export const plantList = [
         id: '3sd',
         light: 1,
         water: 2,
-        cover: monstera,
+        cover: pothos,
     },
     {
-        name: 'yucca',
+        name: 'calathea',
         category: 'classique',
         id: '4kk',
-        light: 3,
-        water: 1,
-        cover: monstera,
+        light: 2,
+        water: 3,
+        cover: calathea,
     },
     {
         name: 'olivier',
@@ -40,32 +48,23 @@ export const plantList = [
         id: '5pl',
         light: 3,
         water: 1,
-        cover: monstera,
+        cover: olivier,
     },
     {
-        name: 'géranium',
-        category: 'extérieur',
-        id: '6uo',
+        name: 'cactus',
+        category: 'plante grasse',
+        id: '8fp',
         light: 2,
-        water: 2,
-        cover: monstera,
+        water: 1,
+        cover: cactus,
     },
     {
         name: 'basilic',
         category: 'extérieur',
         id: '7ie',
-        isBestSale: true,
         light: 2,
         water: 3,
-        cover: monstera,
-    },
-    {
-        name: 'aloe',
-        category: 'plante grasse',
-        id: '8fp',
-        light: 2,
-        water: 1,
-        cover: monstera,
+        cover: basil,
     },
     {
         name: 'succulente',
@@ -73,6 +72,15 @@ export const plantList = [
         id: '9vn',
         light: 2,
         water: 1,
-        cover: monstera,
+        cover: succulent,
+    },
+
+    {
+        name: 'menthe',
+        category: 'extérieur',
+        id: '6uo',
+        light: 2,
+        water: 2,
+        cover: mint,
     },
 ];
