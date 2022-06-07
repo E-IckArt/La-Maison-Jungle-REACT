00d84b90f468933ce49a38ef9e1b07875c59b78b
Emilie Ickart <emi.ickart@gmail.com>
2022-06-07 16:48:43 +0200

    Update banner style

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import logo from '../assets/logo.png';
 import '../styles/Banner.css';
 
 const Banner = () => {
-    const title = 'La maison jungle';
+    const title = 'La Maison Jungle';
     const currentMonth = new Date().getMonth();
     const isSpring = currentMonth >= 2 && currentMonth <= 5;
     const recommendation = isSpring ? (
@@ -14,8 +14,14 @@ const Banner = () => {
     return (
         <div className="lmj-banner">
             <div className="lmj-banner-row">
-                <img src={logo} alt="La maison jungle" className="lmj-logo" />
-                <h1 className="lmj-title">{title}</h1>
+                <div className="lmj-banner-brand">
+                    <img
+                        src={logo}
+                        alt="La maison jungle"
+                        className="lmj-logo"
+                    />
+                    <h1 className="lmj-title">{title}</h1>
+                </div>
                 {recommendation}
             </div>
         </div>
